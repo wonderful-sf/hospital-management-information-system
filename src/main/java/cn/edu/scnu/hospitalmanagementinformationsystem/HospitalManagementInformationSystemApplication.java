4a3b4c29f92e253517ccc34b020cf96536f0bbea
FENG <195194491@qq.com>
2026-05-15 17:08:45 +0800

    init

---
+++ src/main/java/cn/edu/scnu/hospitalmanagementinformationsystem/HospitalManagementInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.scnu.hospitalmanagementinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementInformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementInformationSystemApplication.class, args);
+    }
+
+}
